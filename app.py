ebcb5eb4cc73b2101dfac885d718ccdd32aa4dd2
dimzhead <dimzhead@gmail.com>
2020-02-02 19:30:00 -0500

    'created_my_first_Flask_app'

--- app.py
+++ app.py
@@ -21,5 +21,10 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
+@app.route('/')
+def home():
+ return 'My home page' 
+
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(debug=True, host="0.0.0.0", port=8080) 
